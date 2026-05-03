29d5fd4af5368b2e84ce02c7e0930ba86a71709a
purvi1239 <purvijain1239@gmail.com>
2026-05-03 18:22:46 +0530

    update api

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyA3XTdQO3IQafW1e0y9P1lWeucCAT9BoQE",
-  authDomain: "githubanalyzer-57551.firebaseapp.com",
-  projectId: "githubanalyzer-57551",
-  storageBucket: "githubanalyzer-57551.firebasestorage.app",
-  messagingSenderId: "1088435872417",
-  appId: "1:1088435872417:web:4689fc7da31e1134d7c6e0",
-  measurementId: "G-E7GS7CCR12"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
